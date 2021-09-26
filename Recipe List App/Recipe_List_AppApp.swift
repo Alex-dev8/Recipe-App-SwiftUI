8df1445a6c65771fdf891ead1c4fd5f4fd3fa6f9
Alex Cannizzo <cansal89@gmail.com>
2021-09-26 22:02:57 +0700

    Initial Commit

---
+++ Recipe List App/Recipe_List_AppApp.swift
@@ -0,0 +1,18 @@
+//
+//  Recipe_List_AppApp.swift
+//  Recipe List App
+//
+//  Created by Alex Cannizzo on 23/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_List_App: App {
+    var body: some Scene {
+        WindowGroup {
+            RecipeTabView()
+                
+        }
+    }
+}
